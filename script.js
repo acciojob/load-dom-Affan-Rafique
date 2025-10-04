e9bacd28af6e91877cca82938dccb7f7111bd519
Affan-Rafique <affanrafique9@gmail.com>
2025-10-04 13:43:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+ document.addEventListener('DOMContentLoaded', function() {
+      // Write "DOM load success" to the body
+      document.body.textContent = "DOM load success";
